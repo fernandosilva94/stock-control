7601924227096306a4156c1c776a67edd791d453
Fernando Silva <fernandoadmti@hotmail.com>
2024-12-20 18:17:15 -0300

    css e criacao do form de login e de cadastro de usuario

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  loginCard = true;
+  
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log("Dados do formulario de login: ", this.loginForm.value)
+  }
+
+  onSubmitSignUpForm(): void {
+    console.log("Dados do formulario de criação de conta: ", this.signUpForm.value)
+  }
 
 }
