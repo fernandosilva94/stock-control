bd0e68cf04e7741e356382c21028daaab6bec37c
Fernando Silva <fernandoadmti@hotmail.com>
2024-12-20 21:44:15 -0300

    implementacao de criacao de usuario e autenticacao de login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { SignUpUserRequest } from 'src/app/models/interfaces/user/SignUpUserRequest';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -20,14 +24,35 @@ export class HomeComponent {
     password: ['', Validators.required]
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private cookieService: CookieService) {}
 
   onSubmitLoginForm(): void {
-    console.log("Dados do formulario de login: ", this.loginForm.value)
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
+        next: (response) => {
+          if (response) {
+            this.cookieService.set('USER_INFO', response?.token);
+            this.loginForm.reset();
+          }
+        },
+        error: (err) => console.log("error: ", err)
+      })
+    }
   }
 
   onSubmitSignUpForm(): void {
-    console.log("Dados do formulario de criação de conta: ", this.signUpForm.value)
+    if (this.signUpForm.value && this.signUpForm.valid) {
+      this.userService.signUpUser(this.signUpForm.value as SignUpUserRequest).subscribe({
+        next: (response) => {
+          if (response) {
+            alert('Usuário criado com sucesso.');
+            this.signUpForm.reset();
+            this.loginCard = true;
+          }
+        },
+        error: (err) => console.log("error: ", err)
+      })
+    }
   }
 
 }
