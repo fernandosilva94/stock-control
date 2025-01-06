02059edb943eceafc5221223e50870e52fd5acf9
Fernando Silva <fernandoadmti@hotmail.com>
2025-01-06 20:08:37 -0300

    adicionando guarda de rotas

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,7 @@
 import { AuthResponse } from './../../models/interfaces/user/auth/AuthResponse';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { SignUpUserRequest } from 'src/app/models/interfaces/user/SignUpUserRequest';
@@ -13,7 +14,7 @@ import { environments } from 'src/environments/environments';
 export class UserService {
   private API_URL = environments.API_URL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie: CookieService) { }
 
   signUpUser(requestDatas: SignUpUserRequest): Observable<SignUpUserResponse> {
     return this.http.post<SignUpUserResponse>(
@@ -24,5 +25,11 @@ export class UserService {
   authUser(requestDatas: AuthRequest): Observable<AuthResponse> {
    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas); 
   };
+
+  isLoggedIn(): boolean {
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+
+    return JWT_TOKEN ? true : false;
+  }
   
 };
